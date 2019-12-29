77cbb2b5aef5722c3214be6eef1aa40fce9685ae
spnraju <padmanabharaju.sagi@gmail.com>
2019-12-30 03:47:23 +0530

    implement login steps

--- features/step-definitions/home-page.steps.js
+++ features/step-definitions/home-page.steps.js
@@ -1,15 +1,19 @@
 import {Given, When, Then} from 'cucumber';
 import {props} from '../../constants';
 import log4js from 'log4js';
+import assert from 'assert';
 
 const log = log4js.getLogger();
 
-Given(/^user is in homepage of "(.*?)" application$/, (appName) => {
-  console.error(props.EMAIL.ADMIN);
+Given(/^user is in login page of "(.*?)" application$/, (app) => {
+  const url = app === 'frontend' ? props.URL.FRONT_END : props.URL.ADMIN;
+  const expectedText = app === 'frontend' ? props.PAGE_TITLE.FRONT_END : props.PAGE_TITLE.ADMIN;
+  browser.url(url);
+  assert.equal(browser.getTitle(), expectedText); 
 });
 
-When(/^user clicks on "(.*?)" from the menu$/, (link) => {
-  log.info(link);
+When(/^user enters credentials for "(.*?)" application$/, (link) => {
+  browser.$('input').setValue(props.EMAIL.FRONT_END);
 });
 
 Then(/^user expects to be navigated to "(.*?)" page$/, (pageName) => {
