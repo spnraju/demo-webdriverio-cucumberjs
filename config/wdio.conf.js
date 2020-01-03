0020d446e03bf1db3002380c2a207f1fcee9fd06
spnraju <padmanabharaju.sagi@gmail.com>
2020-01-04 03:53:36 +0530

    update config for allure reports

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -50,20 +50,24 @@ exports.config = {
     ['allure', {
       outputDir: 'allure-results',
       disableWebdriverStepsReporting: true,
+      disableWebdriverScreenshotsReporting: false,
+      useCucumberStepReporter: false,
     }],
   ],
   cucumberOpts: {
     requireModule: ['@babel/register'],
     require: ['./features/step-definitions/*.js'], // <string[]> (file/dir) require files before executing features
-    backtrace: false, // <boolean> show full backtrace for errors
+    backtrace: true, // <boolean> show full backtrace for errors
+    failAmbiguousDefinitions: true, // <boolean< Treat ambiguous definitions as errors
     dryRun: false, // <boolean> invoke formatters without executing steps
     failFast: false, // <boolean> abort the run on first failure
+    ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings
     format: ['pretty'], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
     colors: true, // <boolean> disable colors in formatter output
-    snippets: true, // <boolean> hide step definition snippets for pending steps
-    source: true, // <boolean> hide source uris
+    snippets: false, // <boolean> hide step definition snippets for pending steps
+    source: false, // <boolean> hide source uris
     profile: [], // <string[]> (name) specify the profile to use
-    strict: false, // <boolean> fail if there are any undefined or pending steps
+    strict: true, // <boolean> fail if there are any undefined or pending steps
     tagExpression: '', // <string> (expression) only execute the features or scenarios with tags matching the expression
     timeout: 60000, // <number> timeout for step definitions
     ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
