26a446448e71b7e7954dc5cfb35d934f53d88e41
spnraju <padmanabharaju.sagi@gmail.com>
2020-03-29 23:54:15 +0530

    feat: login positive scenario

--- features/step-definitions/home-page.steps.js
+++ features/step-definitions/home-page.steps.js
@@ -8,14 +8,17 @@ Given(/^user is in login page of "(.*?)" application$/, (app) => {
   const url = app === 'frontend' ? props.URL.FRONT_END : props.URL.ADMIN;
   const expectedText = app === 'frontend' ? props.PAGE_TITLE.FRONT_END : props.PAGE_TITLE.ADMIN;
   browser.url(url);
-  log.info('test');
   assert.equal(browser.getTitle(), expectedText); 
 });
 
 When(/^user enters credentials for "(.*?)" application$/, (link) => {
-  browser.$('input').setValue(props.EMAIL.FRONT_END);
+  browser.$('input[name="username"]').setValue(props.EMAIL.FRONT_END);
+  browser.$('input[name="password"]').setValue(props.PASSWORD.FRONT_END);
+  browser.$('.btn.btn-primary.btn-lg.btn-block.loginbtn').click();
 });
 
 Then(/^user expects to be navigated to "(.*?)" page$/, (pageName) => {
-  log.info(pageName);
+  browser.pause(5000);
+  const url = browser.getUrl();
+  assert.equal(url, 'https://www.phptravels.net/account/');
 });
